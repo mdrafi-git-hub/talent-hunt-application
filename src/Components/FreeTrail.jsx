2c2e023c44877b559ab740bbbba3f436696278e4
mdrafi-git-hub <rafimohammad055@gmail.com>
2025-08-04 00:30:50 +0530

    Fixed import path in FreeTrail.jsx

--- src/Components/FreeTrail.jsx
+++ src/Components/FreeTrail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Apply from "../Pages/apply";
+import Apply from "../Pages/Apply";
 import { useNavigate } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
